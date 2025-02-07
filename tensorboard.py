d1a194e27c5f8d6d854b350e02427f5133fc8393
dvtiendat <111187020+dvtiendat@users.noreply.github.com>
2025-02-07 11:08:38 +0700

    tensorboard adjustment

---
+++ tensorboard.py
@@ -0,0 +1,16 @@
+import tensorboard
+from tensorboard import SummaryWriter
+
+writer = SummaryWriter('runs/ukyio_face_cyclegan')
+
+def log_losses(writer, d_loss, g_loss, cycle_loss, identity_loss, step):
+    writer.add_scalar('Discriminator Loss', d_loss, global_step=step)
+    writer.add_scalar('Generator Loss', g_loss, global_step=step)
+    writer.add_scalar('Cycle Loss', cycle_loss, global_step=step)
+    writer.add_scalar('Identity Loss', identity_loss, global_step=step)
+
+def log_images(writer, real_face, real_ukiyo, fake_face, fake_ukiyo, step):
+    writer.add_image('Real Face', real_face, global_step=step)
+    writer.add_image('Real Ukiyo', real_ukiyo, global_step=step)
+    writer.add_image('Fake Face', fake_face, global_step=step)
+    writer.add_image('Fake Ukiyo', fake_ukiyo, global_step=step)
